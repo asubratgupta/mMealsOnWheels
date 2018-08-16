9bd7b1a507433ab312f497e9de49a648543a9217
subratgupta <newsubrat@gmail.com>
2018-08-16 16:38:28 +0530

    Working On Sign In Screen...

--- app/src/main/java/com/subratgupta/mmealsonwheels/MainActivity.java
+++ app/src/main/java/com/subratgupta/mmealsonwheels/MainActivity.java
@@ -2,6 +2,7 @@ package com.subratgupta.mmealsonwheels;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
